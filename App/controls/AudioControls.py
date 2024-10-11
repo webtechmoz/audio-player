cf8fcb1915797d868d4dd4aec2d0bfa7ffcb7b82
webtechmoz <zoidycine@gmail.com>
2024-10-12 00:51:13 +0200

    Audio Player #2

--- App/controls/AudioControls.py
+++ App/controls/AudioControls.py
@@ -3,6 +3,7 @@ from controls.controls import (
     IconButton,
     Text
 )
+import json
 
 class AudioControls(ft.Container):
     def __init__(
@@ -66,27 +67,30 @@ class AudioControls(ft.Container):
                 ),
                 ft.Row(
                     controls=[
-                        IconButton(
+                        btn_playlist := IconButton(
                             icon=ft.icons.MENU,
                             color=ft.colors.WHITE
                         ),
                         ft.Row(
                             controls=[
-                                IconButton(
+                                btn_previous := IconButton(
                                     icon=ft.icons.SKIP_PREVIOUS,
-                                    color=ft.colors.WHITE
+                                    color=ft.colors.WHITE,
+                                    on_click=self.previous
                                 ),
-                                IconButton(
+                                btn_play := IconButton(
                                     icon=ft.icons.PLAY_ARROW,
-                                    color=ft.colors.WHITE
+                                    color=ft.colors.WHITE,
+                                    on_click=self.play
                                 ),
-                                IconButton(
+                               btn_next :=  IconButton(
                                     icon=ft.icons.SKIP_NEXT,
-                                    color=ft.colors.WHITE
+                                    color=ft.colors.WHITE,
+                                    on_click=self.next
                                 )
                             ]
                         ),
-                        IconButton(
+                        btn_favorite := IconButton(
                             icon=ft.icons.FAVORITE_BORDER,
                             color=ft.colors.WHITE
                         ),
@@ -99,6 +103,101 @@ class AudioControls(ft.Container):
 
         self.pg_1 = pg_1
         self.pg_2 = pg_2
+        self.btn_playlist = btn_playlist
+        self.btn_previous = btn_previous
+        self.btn_play = btn_play
+        self.btn_next = btn_next
+        self.btn_favorite = btn_favorite
+        self.audio_playlist: list[ft.Audio] = []
+        self.current_audio: ft.Audio = None
+    
+    def play(self, e: ft.ControlEvent):
+
+        if self.current_audio is None:
+            self.add_playlist()
+
+            if len(self.audio_playlist) > 0:
+                self.current_audio = self.audio_playlist[-1]
+            
+            else:
+                return
+
+        if self.btn_play.icon == 'play_arrow':
+            self.current_audio.resume()
+            self.btn_play.icon = ft.icons.PAUSE
+        
+        elif self.btn_play.icon == 'pause':
+            self.current_audio.pause()
+            self.btn_play.icon = ft.icons.PLAY_ARROW
+        
+        self.page.update()
+    
+    def previous(self, e: ft.ControlEvent):
+        if self.current_audio is not None:
+            self.current_audio.pause()
+            self.btn_play.icon = ft.icons.PLAY_ARROW
+
+            i = self.audio_playlist.index(self.current_audio)
+
+            if i > 0:
+                self.current_audio = self.audio_playlist[i - 1]
+            
+            else:
+                self.current_audio = self.audio_playlist[-1]
+        
+        else:
+            self.add_playlist()
+            self.current_audio = self.audio_playlist[0]
+        
+        self.current_audio.play()
+        self.btn_play.icon = ft.icons.PAUSE
+        self.page.update()
+
+    def next(self, e: ft.ControlEvent):
+        if self.current_audio is not None:
+            self.current_audio.pause()
+            self.btn_play.icon = ft.icons.PLAY_ARROW
+
+            i = self.audio_playlist.index(self.current_audio)
+
+            if i < len(self.audio_playlist) - 1:
+                self.current_audio = self.audio_playlist[i + 1]
+            
+            else:
+                self.current_audio = self.audio_playlist[0]
+        
+        else:
+            self.add_playlist()
+            self.current_audio = self.audio_playlist[0]
+        
+        self.current_audio.play()
+        self.btn_play.icon = ft.icons.PAUSE
+        self.page.update()
+    
+    def add_playlist(self):
+        try:
+            if self.page.platform.value == 'windows':
+                with open('playlist/windows_playlist.json', 'r') as file:
+                    audio_playlist: list[dict[str, str]] = json.load(file)['audios']
+            
+            elif self.page.platform.value == 'android':
+                with open('playlist/android_playlist.json', 'r') as file:
+                    audio_playlist: list[dict[str, str]] = json.load(file)['audios']
+            
+            for audio in audio_playlist:
+                audio_control = ft.Audio(
+                    src=audio['src'],
+                    volume=1,
+                    autoplay=False
+                )
+
+                self.audio_playlist.append(audio_control)
+                self.page.overlay.append(audio_control)
+                self.page.update()
+        
+        except Exception as e:
+            print(f'Erro: {e}')
+
 
 class ProgressBar(ft.Container):
     def __init__(
--- App/controls/FilePicker.py
+++ App/controls/FilePicker.py
@@ -1,15 +1,74 @@
 import flet as ft
+import json
 
 class FilePicker(ft.FilePicker):
     def __init__(
-        self,
+        self
     ):
         super().__init__()
         self.on_result = self.file_picker_result
+        self.audio_files: ft.FilePickerResultEvent = None
     
 
     def file_picker_result(self, e: ft.FilePickerResultEvent):
         if e.files:
-            for file in e.files:
-                print(file.path)
+            self.audio_files = e
         
+        self.add_playlist()
+    
+
+    def add_playlist(self):
+        try:
+            if self.page.platform.value == 'windows':
+                with open('playlist/windows_playlist.json', 'r') as file:
+                    json.load(file)
+            
+            if self.page.platform.value == 'android':
+                with open('playlist/android_playlist.json', 'r') as file:
+                    json.load(file)
+        
+        except:
+            if self.page.platform.value == 'windows':
+                with open('playlist/windows_playlist.json', 'w') as file:
+                    json.dump({"audios": []}, file, indent=4)
+            
+            if self.page.platform.value == 'android':
+                with open('playlist/android_playlist.json', 'w') as file:
+                    json.dump({"audios": []}, file, indent=4)
+        
+        if self.audio_files is not None:
+            # Lê os ficheiros json
+            if self.page.platform.value == 'windows':
+                with open('playlist/windows_playlist.json', 'r') as file:
+                    audio_files = json.load(file)
+            
+            if self.page.platform.value == 'android':
+                with open('playlist/android_playlist.json', 'r') as file:
+                    audio_files = json.load(file)
+            
+            # Adiciona os arquivos
+            if self.page.platform.value == 'windows':
+                with open('playlist/windows_playlist.json', 'w') as file:
+                    for audio_file in self.audio_files.files:
+                        audio_files['audios'].append(
+                            {
+                                'src': audio_file.path,
+                                'name': audio_file.name,
+                                'size': audio_file.size
+                            }
+                        )
+
+                    json.dump(audio_files, file, indent=4)
+            
+            if self.page.platform.value == 'android':
+                with open('playlist/android_playlist.json', 'w') as file:
+                    for audio_file in self.audio_files.files:
+                        audio_files['audios'].append(
+                            {
+                                'src': audio_file.path,
+                                'name': audio_file.name,
+                                'size': audio_file.size
+                            }
+                        )
+
+                    json.dump(audio_files, file, indent=4)
